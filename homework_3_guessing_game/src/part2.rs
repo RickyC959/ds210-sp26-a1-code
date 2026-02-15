08c39296c6192db0a395ef194f958a225da9f3a7
Ricky <rcui1@bu.edu>
2026-02-15 16:33:30 -0500

    soloution for part2

--- homework_3_guessing_game/src/part2.rs
+++ homework_3_guessing_game/src/part2.rs
@@ -3,26 +3,26 @@ use crate::strategies::Strategy;
 
 pub struct Part2 {}
 
-// Terrible strategy: ask if the number is min, otherwise return max.
 impl Strategy for Part2 {
-    fn guess_the_number(player: &mut Player, mut min: u32,  mut max: u32) -> u32 {
-        let mut guess = ((max-min)/2) + min;
-        for  i in min..max{
+    fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
+        let mut tmin = min;
+        let mut tmax = max;
+        let mut guess = ((tmax-tmin)/2) + tmin;
+        loop {
             let value = player.ask_to_compare(guess);
             if value == 0{
                 return guess;
             }
             else if value == 1{ // num is greater
-                min = guess;
+                tmin = guess;
                 
             }
             else if value == -1{ // num is less
-                max = guess;
+                tmax = guess;
                 
             }
-             guess = ((max-min)/2) + min; //set the next guess to the new middle value
+             guess = ((tmax-tmin)/2) + tmin; //set the next guess to the new middle value
             
         } 
-        return 0;
     }
 }
